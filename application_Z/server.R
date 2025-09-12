9d938929fd2435a4a45a185d1eda055f89d5dd71
Xavier Lefrançois <xavier.lefrancois.1@ulaval.ca>
2025-09-11 22:00:48 -0400

    Format

--- application_Z/server.R
+++ application_Z/server.R
@@ -1,18 +1,10 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
+## Paquetages
 library(shiny)
 library(projetShinyACT2004)
 library(ggplot2)
 library(latex2exp)
 
-# Define server logic required to draw a histogram
+## Serveur
 function(input, output, session) {
 
     ## Âge initial
@@ -39,32 +31,55 @@ function(input, output, session) {
 
 
     ## Valeurs intermédiaires
-    the_t <- reactive(seq(0, ifelse(input$the_loi == "De Moivre", the_omega(), 1000) - the_x(), 0.2))
+    the_t <- reactive(seq(0, ifelse(input$the_loi == "De Moivre",
+                                    the_omega(),
+                                    1000) - the_x(), 0.2))
     the_Z <- reactive(Z(the_delta(), the_t()))
     the_tqx <- reactive(switch(input$the_loi,
-                               "De Moivre" = tqx("demoivre", t = the_t(), omega = the_omega(), x = the_x()),
-                               "Exponentielle" = tqx("expo", t = the_t(), mu = the_mu())))
+                               "De Moivre" = tqx("demoivre",
+                                                 t = the_t(),
+                                                 omega = the_omega(),
+                                                 x = the_x()),
+                               "Exponentielle" = tqx("expo",
+                                                     t = the_t(),
+                                                     mu = the_mu())))
     the_max <- reactive(switch(input$the_loi,
                                "De Moivre" = TRUE,
                                "Exponentielle" = the_t() <= 100))
 
     ## Assurance vie entière (valeur actualisée)
     output$Z_ent <- renderPlot({
-        graph_Z(Z = the_Z()[the_max()], t = the_t()[the_max()], n = the_n(), x = the_x(), type = "ent")
+        graph_Z(Z = the_Z()[the_max()],
+                t = the_t()[the_max()],
+                n = the_n(),
+                x = the_x(),
+                type = "ent")
     })
 
     ## Autre produit (valeur actualisée)
     output$Z_comp <- renderPlot({
-        graph_Z(Z = the_Z()[the_max()], t = the_t()[the_max()], n = the_n(), x = the_x(), type = the_type())
+        graph_Z(Z = the_Z()[the_max()],
+                t = the_t()[the_max()],
+                n = the_n(),
+                x = the_x(),
+                type = the_type())
     })
 
     ## Assurance vie entière (fonction de répartition)
     output$FZ_ent <- renderPlot({
-        graph_FZ(Z = the_Z(), t = the_t(), tqx = the_tqx(), n = the_n(), type = "ent")
+        graph_FZ(Z = the_Z(),
+                 t = the_t(),
+                 tqx = the_tqx(),
+                 n = the_n(),
+                 type = "ent")
     })
 
     ## Autre produit (fonction de répartition)
     output$FZ_comp <- renderPlot({
-        graph_FZ(Z = the_Z(), t = the_t(), tqx = the_tqx(), n = the_n(), type = the_type())
+        graph_FZ(Z = the_Z(),
+                 t = the_t(),
+                 tqx = the_tqx(),
+                 n = the_n(),
+                 type = the_type())
     })
 }
